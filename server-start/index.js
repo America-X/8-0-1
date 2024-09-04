c18d8ca771abde39b96e99fb9d727335d70776b7
Sleepy-kookoo <americaxalvarez@gmail.com>
2024-09-04 12:13:05 -0400

    Created path to vite-project

--- server-start/index.js
+++ server-start/index.js
@@ -1,10 +1,17 @@
 const gifs = require('./gifs.json');
 const express = require('express');
 // 1. Import Path
+const path = require('path')
 
 // 2. Define a path to the dist folder
 
 const app = express();
+const pathToDistFolder = path.join(__dirname, '../vite-project/dist')
+console.log(pathToDistFolder)
+
+const serverStatic = express.static(pathToDistFolder)
+
+//dirname is our currently location: index.js(server-start), refers to parent directory
 
 /////////////////////
 // Controllers
@@ -12,6 +19,18 @@ const app = express();
 
 // 3. Create the logRoutes middleware
 
+//Middleware takes in the request and hands it off to someone else, 
+//kinda of like a mailman gets the mail and send it to someone else
+
+// Middleware function for logging route requests
+const logRoutes = (req, res, next) => {
+  // variable holding the time
+  const time = new Date().toLocaleString();
+  // info we are getting from the request along with the time
+  console.log(`${req.method}: ${req.originalUrl} - ${time}`);
+  //m move to the next controller
+  next(); // Passes the request to the next middleware/controller
+};
 
 // 4. Create the serveStatic middleware
 
@@ -29,9 +48,11 @@ const serveHello = (req, res, next) => {
 
 // 5. Use both middleware
 
+app.use(logRoutes)
 
 app.get('/api/hello', serveHello);
 app.get('/api/data', serveData);
+app.use(serverStatic)
 
 
 const port = 8080;
